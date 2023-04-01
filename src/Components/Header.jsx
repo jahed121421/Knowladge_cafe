970f05edfc4883b8ece19ee15aced1e401938153
jahed121421 <nabilahmed121421@gmail.com>
2023-04-01 17:52:38 +0600

    icon edit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-              <img src="../../src/assets/icon.jpg" />
+              <img src="icon.jpg" alt="" />
             </div>
           </label>
         </div>
